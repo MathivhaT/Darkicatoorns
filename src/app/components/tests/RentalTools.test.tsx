7483f08bb3edba5fe6b084d92cd45a00d810724e
Talifhani Mathivha <talifhanim@msbe.co.za>
2024-06-01 08:38:17 +0200

    updated test configs

--- src/app/components/tests/RentalTools.test.tsx
+++ src/app/components/tests/RentalTools.test.tsx
@@ -3,15 +3,6 @@ import { render, waitFor } from '@testing-library/react';
 import RentalTools from '../RentalTools';
 import * as dataService from '../../services/dataService';
 
-jest.mock('../../services/dataService');
-
-const mockRentalToolsData = [
-  { workOrder: '01', toolRef: '6465', teamMember: 'Alex Noman', status: 'Completed', duration: '15:00' },
-  { workOrder: '02', toolRef: '5665', teamMember: 'Razib Rahman', status: 'In Progress', duration: '05:00' },
-  { workOrder: '03', toolRef: '1755', teamMember: 'Luke Norton', status: 'Not Started', duration: '00:00' }
-];
-
-(dataService.fetchRentalTools as jest.Mock).mockResolvedValue(mockRentalToolsData);
 
 test('renders RentalTools component', async () => {
   const { getByText } = render(<RentalTools />);
